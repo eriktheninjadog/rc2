7505511e2fa189ad1140e39ceb66c8a3faaee6d2
eriktheninjadog <erik.tamm@eriktamm.com>
2023-07-04 08:43:33 +0800

    done d

--- bd.sh
+++ bd.sh
@@ -1,2 +1,5 @@
+REACTBUILDVERSION=$(date +%Y-%m-%d)-$(date +%T) &&
+      sed -i -- 's/%REACTBUILDVERSION%/'$REACTBUILDVERSION'/g' build/index.html &&
+      echo React Build Version = $REACTBUILDVERSION",
 npm run build
 scp -r build/* erik@chinese.eriktamm.com:/var/www/html
